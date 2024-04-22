69c5e4671cadc71e81e6f7ac68bbeb8bc3888676
crackalamoo <harysdalvi@gmail.com>
2024-04-22 16:52:47 -0400

    add plots

--- code/main.py
+++ code/main.py
@@ -14,12 +14,13 @@ parser.add_argument("--device")
 parser.add_argument("--epochs")
 args = parser.parse_args()
 
-EPOCHS = int(args.epochs) if args.epochs is not None else 10
+EPOCHS = int(args.epochs) if args.epochs is not None else 20
 device = args.device or "cpu"
+USE_CURRICULUM = True
 LOSS_THRESHOLD = 1.0
 REINTRODUCE_LEARNED = 0.1
 STORED_LOSSES = 3
-LR = 1e-3
+LR = 2e-3
 
 def load_curriculum(train_loader, sample_losses, batch_size=None):
     if batch_size is None: batch_size = train_loader.batch_size
@@ -30,20 +31,22 @@ def load_curriculum(train_loader, sample_losses, batch_size=None):
         if np.mean(sample_losses[idx]) > LOSS_THRESHOLD or np.random.random() < REINTRODUCE_LEARNED:
             keep_idx.add(idx)
     subset = Subset(dataset, list(keep_idx))
-    print("Proportion kept:", len(keep_idx)/len(train_loader))
+    proportion = len(keep_idx)/(len(train_loader)*batch_size)
+    print("Proportion kept:", proportion)
 
     training = DataLoader(subset, batch_size=batch_size, shuffle=True)
-    return training
+    return training, proportion
 
-def train(model, train_loader, val_loader=None, epochs=EPOCHS, use_curriculum=True, lr=LR):
+def train(model, train_loader, val_loader=None, epochs=EPOCHS, use_curriculum=USE_CURRICULUM, lr=LR):
     losses = []
     accuracies = []
     val_losses = []
     val_accuracies = []
     training = train_loader
     sample_losses = defaultdict(lambda: 900*np.ones(STORED_LOSSES))
-    start_time = time.time()
     times = []
+    proportions = []
+    start_time = time.time()
     for epoch in range(epochs):
         model.to(device)
         model.train()
@@ -51,7 +54,10 @@ def train(model, train_loader, val_loader=None, epochs=EPOCHS, use_curriculum=Tr
         accuracy = []
         epoch_losses = []
         if use_curriculum and epoch >= STORED_LOSSES:
-            training = load_curriculum(train_loader, sample_losses)
+            training, proportion = load_curriculum(train_loader, sample_losses)
+            proportions.append(proportion)
+        else:
+            proportions.append(1)
         for inputs, labels, idxs in training:
             inputs, labels = inputs.to(device), labels.to(device)
             optimizer.zero_grad()
@@ -78,7 +84,10 @@ def train(model, train_loader, val_loader=None, epochs=EPOCHS, use_curriculum=Tr
             print(f"Epoch: {epoch}, Training Accuracy: {train_acc}")
         times.append(time.time() - start_time)
     if val_loader is not None:
-        return losses, accuracies, val_losses, val_accuracies, times
+        return {
+            'loss': losses, 'acc': accuracies, 'val_loss': val_losses, 'val_acc': val_accuracies,
+            'times': times, 'prop': proportions
+        }
     return losses, accuracies, times
 
 def test(model, test_loader):
@@ -100,19 +109,67 @@ def main():
     train_loader, test_loader = get_cifar10_data()
 
     model = Cifar10Model()
-    train_loss, train_acc, val_loss, val_acc, train_times = train(model, train_loader, val_loader=test_loader, epochs=EPOCHS)
+    train_res = train(model, train_loader, val_loader=test_loader, epochs=EPOCHS)
+    train_loss, train_acc, val_loss, val_acc, train_times, proportions = (train_res['loss'],
+        train_res['acc'], train_res['val_loss'], train_res['val_acc'],
+        train_res['times'], train_res['prop'])
 
     test_loss, test_acc = test(model, test_loader)
 
     print(f"Final Test Accuracy: {test_acc}")
+    print(f"Total time: {train_times[-1]} s")
 
     plt.figure()
-    plt.plot(train_times, train_loss)
-    plt.plot(train_times, val_loss)
+    if USE_CURRICULUM:
+        plt.title('Dynamic curriculum learning avoids overfitting on CIFAR-10')
+    else:
+        plt.title('Model performance without dynamic curriculum')
+    plt.plot(train_times, train_loss, label='training loss')
+    plt.plot(train_times, val_loss, label='validation loss')
+    plt.legend()
+    plt.xlabel('Time (s)')
+    plt.ylabel('Cross-entropy loss')
+    if USE_CURRICULUM:
+        plt.savefig('../results/cur_train_val.png')
+    else:
+        plt.savefig('../results/reg_train_val.png')
 
     plt.figure()
     plt.plot(train_times, train_acc)
     plt.plot(train_times, val_acc)
+    plt.plot(train_times[STORED_LOSSES:], proportions[STORED_LOSSES:])
+
+    fname = 'cur' if USE_CURRICULUM else 'reg'
+    with open(f'../results/{fname}.npy', 'wb') as f:
+        np.save(f, train_times)
+        np.save(f, val_loss)
+        np.save(f, val_acc)
+    
+    if USE_CURRICULUM:
+        try:
+            with open(f'../results/reg.npy', 'rb') as f:
+                reg_times = np.load(f)
+                reg_loss = np.load(f)
+                reg_acc = np.load(f)
+            plt.figure()
+            plt.plot(train_times, val_acc, label='curriculum')
+            plt.plot(reg_times, reg_acc, label='no curriculum')
+            plt.plot(train_times[STORED_LOSSES:], proportions[STORED_LOSSES:], label='proportion')
+            plt.xlabel('Time (s)')
+            plt.ylabel('Accuracy/proportion')
+            plt.title('Faster convergence and comparable performance on CIFAR-10')
+            plt.legend()
+            plt.savefig('../results/cur_vs_reg.png')
+
+            plt.figure()
+            plt.plot(train_times, val_loss, label='curriculum')
+            plt.plot(reg_times, reg_loss, label='no curriculum')
+            plt.xlabel('Time (s)')
+            plt.ylabel('Cross-entropy loss')
+            plt.title('Faster convergence and comparable performance on CIFAR-10')
+            plt.legend()
+            plt.savefig('../results/cur_vs_reg_loss.png')
+        except FileNotFoundError: pass
 
     plt.show()
 
