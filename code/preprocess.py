f5f24e4b22bacb526336538542ac18a41901cfff
crackalamoo <harysdalvi@gmail.com>
2024-04-29 20:36:14 -0400

    pickle WMT dataset

--- code/preprocess.py
+++ code/preprocess.py
@@ -5,7 +5,14 @@ import torch
 from torch.utils.data import TensorDataset, DataLoader
 import torchvision
 from torchvision import transforms
+from datasets import load_dataset
+import spacy
 import shutil
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset")
+args = parser.parse_args()
 
 
 def unpickle(file):
@@ -96,5 +103,85 @@ def get_image_classifier_data(file_path, classes=None, num_channels=3, image_siz
 
     return data_loader
 
+def pickle_wmt(num_train_samples=3000, vocab_size=512):
+    en_tok = spacy.load('en_core_web_sm')
+    fr_tok = spacy.load('fr_core_news_sm')
+
+    dataset = load_dataset('wmt/wmt14', data_dir='fr-en', split='train', streaming=True)
+    english_spacy = []
+    french_spacy = []
+    en_toks = {}
+    fr_toks = {}
+    en_freqs = {}
+    fr_freqs = {}
+    for i, row in enumerate(iter(dataset)):
+        translation = row['translation']
+        en_i = en_tok(translation['en'].lower())
+        fr_i = fr_tok(translation['fr'].lower())
+        for token in en_i:
+            en_freqs[token.text] = en_freqs.get(token.text, 0) + 1
+        for token in fr_i:
+            fr_freqs[token.text] = fr_freqs.get(token.text, 0) + 1
+        english_spacy.append(en_i)
+        french_spacy.append(fr_i)
+        if i == num_train_samples-1:
+            break
+    
+    en_freqs = sorted(en_freqs.items(), key=lambda x: x[1], reverse=True)
+    fr_freqs = sorted(fr_freqs.items(), key=lambda x: x[1], reverse=True)
+    for i in range(vocab_size):
+        en_toks[en_freqs[i][0]] = i
+        fr_toks[fr_freqs[i][0]] = i
+    
+    english = []
+    french = []
+    for i in range(num_train_samples):
+        en_i = []
+        fr_i = []
+        for token in english_spacy[i]:
+            if token.text not in en_toks:
+                en_i.append(len(en_toks))
+            else:
+                en_i.append(en_toks[token.text])
+        for token in french_spacy[i]:
+            if token.text not in fr_toks:
+                fr_i.append(len(fr_toks))
+            else:
+                fr_i.append(fr_toks[token.text])
+        english.append(en_i)
+        french.append(fr_i)
+
+    trainset = {b'data': english, b'labels': french, b'data_tok': en_toks, b'labels_tok': fr_toks}
+    with open('../data/wmt_train', 'wb') as fo:
+        pickle.dump(trainset, fo)
+
+    dataset = load_dataset('wmt/wmt14', data_dir='fr-en', split='validation', streaming=True)
+    english = []
+    french = []
+    for i, row in enumerate(iter(dataset)):
+        translation = row['translation']
+        en_tokenized = en_tok(translation['en'].lower())
+        fr_tokenized = fr_tok(translation['fr'].lower())
+        en_i = []
+        fr_i = []
+        for token in en_tokenized:
+            if token.text not in en_toks:
+                en_i.append(len(en_toks))
+            else:
+                en_i.append(en_toks[token.text])
+        for token in fr_tokenized:
+            if token.text not in fr_toks:
+                fr_i.append(len(fr_toks))
+            else:
+                fr_i.append(fr_toks[token.text])
+        english.append(en_i)
+        french.append(fr_i)
+    testset = {b'data': english, b'labels': french}
+    with open('../data/wmt_test', 'wb') as fo:
+        pickle.dump(testset, fo)
+
 if __name__ == '__main__':
-    pickle_mnist()
+    if args.dataset == 'mnist':
+        pickle_mnist()
+    elif args.dataset == 'wmt':
+        pickle_wmt()
